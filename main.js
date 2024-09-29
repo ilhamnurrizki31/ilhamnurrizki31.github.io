85da47d6e61b0f3c90495444e0e2a75b5f8f4aa7
Ilham Nur Rizki <ilham@192.168.1.3>
2024-09-29 22:33:11 +0700

    Create fitur send email from form contact and create thanks you page

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active');
 }
+
+
+const contactForm = document.getElementById("contact-form")
+const loader = document.querySelector(".loader");
+
+loader.style.display = "none";
+
+contactForm.addEventListener("submit", function (e) {
+    loader.style.display = "block";
+    e.preventDefault();
+    
+    const url = e.target.action; 
+    const formData = new FormData(contactForm)
+
+    fetch(url, {
+        method: "POST", 
+        body: formData, 
+        mode: "no-cors",
+    })
+    .then(() => {
+        //url page thanks you
+        loader.style.display = "none";
+        window.location.href = "/thankyou.html"
+    })
+    .catch((e) => alert("Eror page form"))
+})
+
+
